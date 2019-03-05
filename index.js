2de018eb94dbb13063f596831c33d3960938d535
jyootai <jyootg@gmail.com>
2019-03-05 17:14:17 +0800

    Repairing multi-valued computing errors

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 module.exports = (css, settings) => {
-  const reg = new RegExp(/(\d+)(px;)(.*no)*/g);
+  const reg = new RegExp(/(\d+px)(.*\*no.*)*/g);
   const remUnit = settings.remUnit || 75;
+  const precision = settings.precision || 6;
   return css.
-  replace(reg, (match, $1, $2, $3) => {
-    if ($3) {
-      return match;
+  replace(reg, matched => {
+    if (matched.match(/no\*/)) {
+      return matched;
     }
-    const res = $1 / remUnit + 'rem';
-    return `${res};`;
+    const result = matched.replace(/(\d+)px/g, (str, $1) => {
+      const val = Number($1);
+      if (val === 0) {
+        return '0rem';
+      }
+      return ($1 / remUnit).toFixed(precision) + 'rem';
+    });
+    return `${result}`;
   });
 }
